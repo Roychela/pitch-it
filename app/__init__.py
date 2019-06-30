09bffd643dd2a675a1abb23f9008b1ae67f2e7a8
Roy Chela <roychela@gmail.com>
2019-06-30 18:05:04 +0300

    Add email message functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     #Registering authentication blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
