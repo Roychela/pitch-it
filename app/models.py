02bca606753dafe267148b35610d27f509884db1
Roy Chela <roychela@gmail.com>
2019-06-30 19:48:48 +0300

    Add comments class

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(UserMixin, db.Model):
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
     pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comments',backref='user',lazy='dynamic')
 
     @property
     def password(self):
@@ -60,3 +61,20 @@ class Pitch(db.Model):
     def get_pitches(cls,category):
         pitches = Pitch.query.filter_by(category=category).all()
         return pitches
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch):
+        comments = Comments.query.filter_by(pitch_id=id).all()
+        return comments        
