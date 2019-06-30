61e2e813901e49cc18b88a32904413faae319741
Roy Chela <roychela@gmail.com>
2019-06-30 15:44:22 +0300

    Initialize flask application and database mapper

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from Flask import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    return app
