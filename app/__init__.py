7b1d68dd096646ef2fd51b7755b9a81ff237fb20
Roy Chela <roychela@gmail.com>
2019-06-30 18:10:34 +0300

    Initialize the flask login manager and flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_mail import Mail
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -17,6 +22,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
     #Registering authentication blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
